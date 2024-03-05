b4bf1b71dedf545b084e8709b1c65e5c0fbecb4e
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-05 08:04:34 -0300

    Criando o projeto

---
+++ src/main/java/br/com/api/vspet/VspetApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.vspet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VspetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VspetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/vspet/VspetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.vspet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VspetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
