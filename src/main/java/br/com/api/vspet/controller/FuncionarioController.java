c6069420a599d5ba1351e6112edd99049d9c6b8f
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-05-10 00:22:01 -0300

    Adicionando e personalizando o swagger no projeto

--- src/main/java/br/com/api/vspet/controller/FuncionarioController.java
+++ src/main/java/br/com/api/vspet/controller/FuncionarioController.java
@@ -6,9 +6,14 @@ import br.com.api.vspet.model.funcionario.FuncionarioResponseDTO;
 import br.com.api.vspet.model.funcionario.FuncionarioUpdateDTO;
 import br.com.api.vspet.repository.FuncionarioRepository;
 import br.com.api.vspet.repository.PetshopRespository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,6 +27,7 @@ import java.net.URI;
 @RestController
 @RequestMapping("/funcionario")
 @Slf4j
+@Tag(name = "funcionários", description = "Endpoint relacionado aos dados de funcionários")
 public class FuncionarioController {
 
     @Autowired
@@ -31,13 +37,19 @@ public class FuncionarioController {
     private PetshopRespository petshopRespository;
 
     @GetMapping
-    public ResponseEntity<Page<FuncionarioResponseDTO>> index(@PageableDefault(size = 10, sort = {"nome"}, direction = Sort.Direction.DESC) Pageable pageable) {
+    @Operation(summary = "Lista todos os funcionários", description = "Endpoint retorna de forma paginada todos os funcionários cadastrados, por padrão cada pagina contém 10 funcionários ordenados por nome, mas esses dados de paginação podem ser personalizados.")
+    public ResponseEntity<Page<FuncionarioResponseDTO>> index(@ParameterObject @PageableDefault(size = 10, sort = {"nome"}, direction = Sort.Direction.DESC) Pageable pageable) {
         log.info("listando todos os funcionarios");
         var page = repository.findAll(pageable).map(FuncionarioResponseDTO::new);
         return ResponseEntity.ok(page);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Exibe os detalhes do funcionário de id equivalente", description = "Endpoint retorna um objeto contendo os dados não sensíveis de um funcionário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Funcionário não encontrado"),
+            @ApiResponse(responseCode = "200", description = "Funcionário detalhado com sucesso!")
+    })
     public ResponseEntity<FuncionarioResponseDTO> get(@PathVariable Long id){
         log.info("buscar funcionario com o id: {}",id);
         var funcionario = repository.getReferenceById(id);
@@ -46,7 +58,12 @@ public class FuncionarioController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity create(@RequestBody @Valid FuncionarioInputDTO funcionarioInput){
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Erro de validação nos dados do funcionário"),
+            @ApiResponse(responseCode = "201", description = "Funcionário cadastrado com sucesso!")
+    })
+    @Operation(summary = "Cadastra um novo funcionário no sistema", description = "Endpoint recebe no corpo da requisição um objeto contendo os dados necessários para realizar o cadastro de um novo funcionário")
+    public ResponseEntity create(@ParameterObject @RequestBody @Valid FuncionarioInputDTO funcionarioInput){
         log.info("cadastrando funcionario: {}", funcionarioInput);
         var petshop = petshopRespository.getReferenceById(funcionarioInput.id_petshop());
         var funcionarioSaved = repository.save(new Funcionario(petshop,funcionarioInput));
@@ -56,6 +73,11 @@ public class FuncionarioController {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Funcionário não encontrado"),
+            @ApiResponse(responseCode = "204", description = "Funcionário removido com sucesso!")
+    })
+    @Operation(summary = "Deleta um funcionário do sistema", description = "Endpoint recebe no path o id do funcionário a ser deletado")
     public ResponseEntity delete(@PathVariable Long id){
         log.info("deletando funcionario com o id: {}",id);
         var funcionario = repository.getReferenceById(id);
@@ -65,7 +87,12 @@ public class FuncionarioController {
 
     @PutMapping("/{id}")
     @Transactional
-    public ResponseEntity<FuncionarioResponseDTO> update(@PathVariable Long id, @RequestBody @Valid FuncionarioUpdateDTO dadosFuncionario){
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Erro de validação nos dados do funcionário"),
+            @ApiResponse(responseCode = "200", description = "Funcionário atualizado com sucesso!")
+    })
+    @Operation(summary = "Atualiza um novo funcionário do sistema", description = "Endpoint recebe no corpo da requisição um objeto contendo os dados necessários para atualizar os dados de um funcionário")
+    public ResponseEntity<FuncionarioResponseDTO> update(@PathVariable Long id, @ParameterObject @RequestBody @Valid FuncionarioUpdateDTO dadosFuncionario){
         log.info("atualizando os dados do funcionario com o id: {}", id);
         var funcionario = repository.getReferenceById(id);
         funcionario.update(dadosFuncionario);
--- src/main/java/br/com/api/vspet/controller/PetController.java
+++ src/main/java/br/com/api/vspet/controller/PetController.java
@@ -11,9 +11,14 @@ import br.com.api.vspet.model.pet.PetUpdateDTO;
 import br.com.api.vspet.model.petShop.PetShop;
 import br.com.api.vspet.repository.PetRepository;
 import br.com.api.vspet.repository.PetshopRespository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -29,6 +34,7 @@ import java.util.List;
 @RequestMapping("/pets")
 @Slf4j
 @CrossOrigin("*")
+@Tag(name = "pets", description = "Endpoint relacionado aos dados de pets")
 public class PetController {
 
     @Autowired
@@ -38,6 +44,7 @@ public class PetController {
     private PetshopRespository petshopRespository;
 
     @GetMapping
+    @Operation(summary = "Lista todos os pets", description = "Endpoint retorna de forma paginada todos os pets cadastrados, por padrão cada pagina contém 10 pets ordenados por nome, mas esses dados de paginação podem ser personalizados.")
     public ResponseEntity<Page<PetResponseDTO>> index(
             @PageableDefault(size = 10, sort = {"nome"}, direction = Sort.Direction.DESC) Pageable pageable
     ) {
@@ -49,6 +56,11 @@ public class PetController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Exibe os detalhes do pet de id equivalente", description = "Endpoint retorna um objeto contendo os dados não sensíveis de um pet")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "pet não encontrado"),
+            @ApiResponse(responseCode = "200", description = "pet detalhado com sucesso!")
+    })
     public ResponseEntity<PetResponseDTO> get(@PathVariable Long id){
         log.info("buscar pet com o id: {}",id);
 
@@ -58,6 +70,11 @@ public class PetController {
     }
 
     @GetMapping("/cpf/{cpf}")
+    @Operation(summary = "Exibe os pets que correspondem ao cpf de seus donos", description = "Endpoint retorna uma lista de pets")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "pets não encontrado"),
+            @ApiResponse(responseCode = "200", description = "pets detalhados com sucesso!")
+    })
     public ResponseEntity<List<PetResponseDTO>> getByCpf(@PathVariable String cpf){
         log.info("buscar pet com o cpf: {}",cpf);
 
@@ -68,7 +85,12 @@ public class PetController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity create(@RequestBody @Valid PetInputDto petInputDto){
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Erro de validação nos dados do pet"),
+            @ApiResponse(responseCode = "201", description = "pet cadastrado com sucesso!")
+    })
+    @Operation(summary = "Cadastra um novo pet no sistema", description = "Endpoint recebe no corpo da requisição um objeto contendo os dados necessários para realizar o cadastro de um novo pet")
+    public ResponseEntity create(@ParameterObject @RequestBody @Valid PetInputDto petInputDto){
         log.info("cadastrando pet: {}", petInputDto);
 
         PetShop petshop = petshopRespository.getReferenceById(petInputDto.id_petshop());
@@ -80,6 +102,11 @@ public class PetController {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "pet não encontrado"),
+            @ApiResponse(responseCode = "204", description = "pet removido com sucesso!")
+    })
+    @Operation(summary = "Deleta um pet do sistema", description = "Endpoint recebe no path o id do pet a ser deletado")
     public ResponseEntity delete(@PathVariable Long id){
         log.info("deletando pet com o id: {}",id);
 
@@ -91,7 +118,13 @@ public class PetController {
 
     @PutMapping("/{id}")
     @Transactional
-    public ResponseEntity<PetResponseDTO> update(@PathVariable Long id, @RequestBody @Valid PetUpdateDTO petUpdateDTO){
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Erro de validação nos dados do pet"),
+            @ApiResponse(responseCode = "201", description = "pet atualizado com sucesso!")
+    })
+    @Operation(summary = "Atualiza um pet no sistema", description = "Endpoint recebe no corpo da requisição um objeto contendo os dados necessários para atualizar os dados de um pet")
+
+    public ResponseEntity<PetResponseDTO> update(@PathVariable Long id, @ParameterObject @RequestBody @Valid PetUpdateDTO petUpdateDTO){
         log.info("atualizando os dados do pet com o id: {}", id);
 
         Pet pet = repository.getReferenceById(id);
